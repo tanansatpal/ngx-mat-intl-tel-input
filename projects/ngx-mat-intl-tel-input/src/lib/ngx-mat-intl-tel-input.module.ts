d34e404ce864dad866bb35920e876fdff5a79ebb
Ben Davies <ben.davies@smartbnb.io>
2019-07-22 16:14:37 +0100

    Basic implementation of search filtering on country names

--- projects/ngx-mat-intl-tel-input/src/lib/ngx-mat-intl-tel-input.module.ts
+++ projects/ngx-mat-intl-tel-input/src/lib/ngx-mat-intl-tel-input.module.ts
@@ -3,9 +3,13 @@ import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NgxMatIntlTelInputComponent } from './ngx-mat-intl-tel-input.component';
 import {MatButtonModule, MatInputModule, MatMenuModule, MatDividerModule} from '@angular/material';
+import { SearchPipe } from './search.pipe';
 
 @NgModule({
-  declarations: [NgxMatIntlTelInputComponent],
+  declarations: [
+    NgxMatIntlTelInputComponent,
+    SearchPipe,
+  ],
   imports:[
     CommonModule,
     FormsModule,
---
+++ projects/ngx-mat-intl-tel-input/src/lib/search.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+
+  // country | search:'searchCriteria'
+  transform(country: any, searchCriteria?: any): any {
+    console.log('tick');
+    if (!searchCriteria || searchCriteria === '') {
+      return true;
+    }
+
+    return country.toLowerCase().includes(searchCriteria.toLowerCase());
+  }
+
+}
