d638165d3408459ebdfa628afcc0d86e7b725fe5
Baltazar <baltazar@bieniek.org.pl>
2020-02-08 22:19:25 +0100

    Updated to Angular 9. Adjusted tests to Angular 9

--- projects/ngx-mat-intl-tel-input-tester/src/app/app.module.ts
+++ projects/ngx-mat-intl-tel-input-tester/src/app/app.module.ts
@@ -1,10 +1,14 @@
+import { MatDividerModule } from '@angular/material/divider';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import {NgxMatIntlTelInputModule} from '../../../ngx-mat-intl-tel-input/src/lib/ngx-mat-intl-tel-input.module';
-import {MatButtonModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -18,6 +22,8 @@ import {MatButtonModule, MatFormFieldModule, MatInputModule} from '@angular/mate
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatMenuModule,
+    MatDividerModule,
     NgxMatIntlTelInputModule
   ],
   providers: [],
--- projects/ngx-mat-intl-tel-input/src/lib/ngx-mat-intl-tel-input.module.ts
+++ projects/ngx-mat-intl-tel-input/src/lib/ngx-mat-intl-tel-input.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NgxMatIntlTelInputComponent } from './ngx-mat-intl-tel-input.component';
-import {MatButtonModule, MatInputModule, MatMenuModule, MatDividerModule} from '@angular/material';
 import { SearchPipe } from './search.pipe';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
