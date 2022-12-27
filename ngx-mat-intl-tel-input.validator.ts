bb20caab470d3b538a34f8658f263404aa65fefe
Pavlo <p.kozachuk@gmail.com>
2022-12-27 12:05:24 +0200

    update validator

--- ngx-mat-intl-tel-input.validator.ts
+++ ngx-mat-intl-tel-input.validator.ts
@@ -1,12 +1,12 @@
-import {UntypedFormControl} from '@angular/forms';
-import {parsePhoneNumber, PhoneNumber} from 'libphonenumber-js';
+import { UntypedFormControl } from '@angular/forms';
+import { parsePhoneNumberWithError, PhoneNumber } from 'libphonenumber-js';
 
 export const phoneNumberValidator = (control: UntypedFormControl) => {
   const error = {validatePhoneNumber: true};
   let numberInstance: PhoneNumber;
   if (control.value) {
     try {
-      numberInstance = parsePhoneNumber(control.value);
+      numberInstance = parsePhoneNumberWithError(control.value);
     } catch (e) {
       control.setValue(null);
       return error;
@@ -22,3 +22,4 @@ export const phoneNumberValidator = (control: UntypedFormControl) => {
   }
   return null;
 };
+
